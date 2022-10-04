a1899489671635a45beb330ce3d4490227bdd70f
deyviddfs <deyvid.dfs@gmail.com>
2022-10-04 20:33:31 -0300

    3SIA

--- app/src/main/java/br/com/deyvidfernandes/feedbackwebservice/network/Card.kt
+++ app/src/main/java/br/com/deyvidfernandes/feedbackwebservice/network/Card.kt
@@ -10,7 +10,5 @@ data class Card (
             @Json(name = "hp") val hp: String = "",
             @Json(name = "images") val images: CardImages,
             @Json(name = "attacks") val attacks: List<CardAttacks>,
-            @Json(name = "subtypes") val subtypes: List<String> = listOf(),
-            @Json(name = "types") val types: List<String> = listOf(),
-            @Json(name = "rules") val rules: List<String> = listOf()
+            @Json(name = "types") val types: List<String> = listOf()
         )
--- app/src/main/java/br/com/deyvidfernandes/feedbackwebservice/network/PokemonApiService.kt
+++ app/src/main/java/br/com/deyvidfernandes/feedbackwebservice/network/PokemonApiService.kt
@@ -5,8 +5,16 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.*
+import java.security.NoSuchAlgorithmException
+
+import java.security.KeyManagementException
+
+import okhttp3.OkHttpClient
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
 
 private const val URL = "https://api.pokemontcg.io/v2/"
 
@@ -15,6 +23,7 @@ private val moshi = Moshi.Builder()
                         .build()
 
 private val retrofit = Retrofit.Builder()
+                        .client(getUnsafeOkHttpClient())
                         .addConverterFactory(MoshiConverterFactory.create(moshi))
                         .baseUrl(URL)
                         .build()
@@ -30,5 +39,47 @@ interface PokemonApiService {
 object PokemonApi{
     val RETROFIT_SERVICE: PokemonApiService by lazy {
         retrofit.create(PokemonApiService::class.java)
+    }
+}
+
+private fun getUnsafeOkHttpClient(): OkHttpClient? {
+    return try {
+        // Create a trust manager that does not validate certificate chains
+        val trustAllCerts = arrayOf<TrustManager>(
+            object : X509TrustManager {
+                @Throws(CertificateException::class)
+                override fun checkClientTrusted(
+                    chain: Array<X509Certificate>,
+                    authType: String
+                ) {
+                }
+
+                @Throws(CertificateException::class)
+                override fun checkServerTrusted(
+                    chain: Array<X509Certificate>,
+                    authType: String
+                ) {
+                }
+
+                override fun getAcceptedIssuers(): Array<X509Certificate> {
+                    return arrayOf()
+                }
+            }
+        )
+
+        // Install the all-trusting trust manager
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+
+        // Create an ssl socket factory with our all-trusting manager
+        val sslSocketFactory = sslContext.socketFactory
+        val builder = OkHttpClient.Builder()
+        builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+        builder.hostnameVerifier { hostname: String?, session: SSLSession? -> true }
+        builder.build()
+    } catch (e: KeyManagementException) {
+        throw RuntimeException(e)
+    } catch (e: NoSuchAlgorithmException) {
+        throw RuntimeException(e)
     }
 }
